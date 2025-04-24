8abeef46d61dfb8ffa9300a6019ce8003875a3e7
Kamenleon <leon.kunishi@gmail.com>
2025-04-24 22:05:25 +0900

    mod: Lintに指摘されたとおりに修正

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,4 +1,4 @@
-! /user/bin/env bash
+#!/usr/bin/env bash
 set -ue
 
 helpmsg() {
@@ -8,30 +8,33 @@ helpmsg() {
 
 link_to_homedir() {
   command echo "backup old dotfiles..."
-  if [ ! -d "$HOME/.dotbackup" ];then
+  if [ ! -d "$HOME/.dotbackup" ]; then
     command echo "$HOME/.dotbackup not found. Auto Make it"
     command mkdir "$HOME/.dotbackup"
   fi
 
-  local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-  local dotdir=$(dirname ${script_dir})
+  local script_dir
+  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+  local dotdir
+  dotdir=$(dirname "$script_dir")
+
   if [[ "$HOME" != "$dotdir" ]]; then
-    for f in $dotdir/.??*; do
-      [[ `basename $f` == ".git" ]] && continue 
-      if [[ -L "$HOME/`basename $f`" ]];then
-        command rm -f "$HOME/`basename $f`"
+    for f in "$dotdir"/.??*; do
+      [[ "$(basename "$f")" == ".git" ]] && continue 
+      if [[ -L "$HOME/$(basename "$f")" ]]; then
+        command rm -f "$HOME/$(basename "$f")"
       fi
-      if [[ -e "$HOME/`basename $f`" ]];then
-        command mv "$HOME/`basename $f`" "$HOME/.dotbackup"
+      if [[ -e "$HOME/$(basename "$f")" ]]; then
+        command mv "$HOME/$(basename "$f")" "$HOME/.dotbackup"
       fi
-      command ln -snf $f $HOME
+      command ln -snf "$f" "$HOME"
     done
   else
     command echo "same install src dest"
   fi
 }
 
-while [ $# -gt 0 ];do
+while [ $# -gt 0 ]; do
   case ${1} in
     --debug|-d)
       set -uex
@@ -47,6 +50,5 @@ while [ $# -gt 0 ];do
 done
 
 link_to_homedir
-git config --global include.path "~/.gitconfig_shared"
+git config --global include.path "$HOME/.gitconfig_shared"
 command echo -e "\e[1;36m Install completed!!!! \e[m"
-
